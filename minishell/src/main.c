aa95ca901ce96cf28f54ffd595a08edce8a6cd83
Gerry Shona <gshona@ge-d6.kzn.21-school.ru>
2020-12-24 13:04:24 +0300

    auto commit

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gshona <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 11:22:18 by gshona            #+#    #+#             */
-/*   Updated: 2020/12/24 13:02:56 by gshona           ###   ########.fr       */
+/*   Updated: 2020/12/24 13:04:18 by gshona           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int		main(int ac, char **av, char **env)
 	ft_printf("%d %s %s\n", ac, av[0], env[0]);
 	print_env(tmp);
 	free_nullterm_char_arr(&tmp);
+	ft_printf( "hello world");
 
 	ft_printf(">>> |%s|\n", get_env_value(env,"TERM_PROGRAMa"));
 
