a62928adcb05d10fba4beb34f6362d66e94b1b33
Gerry Shona <gshona@un-g6.kzn.21-school.ru>
2020-12-26 12:48:26 +0300

    pipes example

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: gshona <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 11:22:18 by gshona            #+#    #+#             */
-/*   Updated: 2020/12/24 13:16:05 by gshona           ###   ########.fr       */
+/*   Updated: 2020/12/26 12:47:40 by gshona           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,68 @@ void	shell_loop()
 }
 
 
+int		exec_command(char *command, char **av, char **env)
+{
+	int	pid;
+	int	status;
+	int	ret;
+
+	ret = 0;
+	pid = fork();
+	if (!pid)
+	{
+			ret = execve(command, av, env);
+			ft_printf("execve returned: %d\n", ret);
+			exit(ret);
+	}
+	else
+		wait(&status);
+	return (ret);
+}
+
+char	**argv_gen(char *command, char *av_1)
+{
+	char **res;
+	res = malloc(sizeof(char*) * 3);
+	res[2] = NULL;
+	res[1] = ft_strdup(av_1);
+	res[0] = ft_strdup(command);
+	return (res);
+}
+
+int		exec_piped_commands(char **av1, char **av2, char **env)
+{
+	int	pid;
+	int	fds[2];
+
+	pipe(fds);
+	pid = fork();
+	if (pid == -1)
+	{
+		ft_printf("error in fork\n");
+		return (1);
+	}
+	else if (pid == 0)
+	{
+		close(fds[1]);
+        dup2(fds[0], 0);
+		execve(av2[0], av2, env);
+	}
+	else
+	{
+		close(fds[0]);
+        dup2(fds[1], 1);
+		execve(av1[0], av1, env);
+	}
+}
+
 int		main(int ac, char **av, char **env)
 {
-	char	**tmp;
+	int tmp[2];
 
-	tmp = copy_env(env);
-	ft_printf("%d %s %s\n", ac, av[0], env[0]);
-	print_env(tmp);
-	free_nullterm_char_arr(&tmp);
-	ft_printf( "hello world");
-
-	ft_printf(">>> |%s|\n", get_env_value(env,"TERM_PROGRAMa"));
-
-
+	//exec_command("/bin/cat", argv_gen("/bin/cat", "src/main.c"), env);
+	//exec_command("/bin/cat", av, env);
+	//
+	exec_piped_commands(argv_gen("/bin/cat", "src/main.c"), argv_gen("/usr/bin/wc", "-l"), env);
+	ft_printf("hello\n");
 }
