3eeed086890652442284c5e709deaa318cfe29f6
Gerry Shona <gshona@pr-a5.kzn.21-school.ru>
2021-01-03 20:09:01 +0300

    norminetted cd builtin

--- minishell/src/cd_builtin.c
+++ minishell/src/cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gshona <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 13:35:21 by gshona            #+#    #+#             */
-/*   Updated: 2021/01/03 19:57:49 by gshona           ###   ########.fr       */
+/*   Updated: 2021/01/03 20:06:15 by gshona           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 static int	replace_or_create_env_variable(t_env *env, char *key, char *value)
 {
 	if (find_env_variable(env, key) == -1)
-		add_env_variable(env, key, value);
+		add_env_variable(env, key, ft_strdup(value));
 	else
-		replace_env_variable(env, key, value);
+		replace_env_variable(env, key, ft_strdup(value));
 	return (0);
+}
+
+static int	return_err(void)
+{
+	print_error("minishell: cd: HOME not set\n");
+	return (1);
+}
+
+static void	replace_pwds(t_env *env, char *pwd, char *oldpwd)
+{
+	replace_or_create_env_variable(env, "OLDPWD", oldpwd);
+	replace_or_create_env_variable(env, "PWD", pwd);
 }
 
 int			cd_biultin(char *const argv[], t_env *env)
@@ -34,25 +46,17 @@ int			cd_biultin(char *const argv[], t_env *env)
 	if (!argv[1])
 	{
 		if (!find_env_variable_cb(env, "HOME", &home))
-			{
-				print_error("minishell: cd: HOME not set\n");
-				return (1);
-			}
+			return (return_err());
 		path = home;
 	}
 	oldpwd = pwd_function();
 	ret = chdir(path);
 	pwd = pwd_function();
 	if (ret)
-	{
 		print_error3("cd", argv[1], strerror(errno));
-		free(pwd);
-		free(oldpwd);
-	}
 	else
-	{	
-		replace_or_create_env_variable(env, "OLDPWD", oldpwd);
-		replace_or_create_env_variable(env, "PWD", pwd);
-	}
+		replace_pwds(env, pwd, oldpwd);
+	free(pwd);
+	free(oldpwd);
 	return ((ret == 0) ? 0 : 1);
 }
