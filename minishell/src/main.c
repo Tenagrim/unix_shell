66a9c1e342042a3b453c0183167bb0ac4139a93e
Viktor Bezbatchenko <tiyez.gr@gmail.com>
2021-01-03 14:47:15 +0300

    func_ptr

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jsandsla <jsandsla@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 11:22:18 by gshona            #+#    #+#             */
-/*   Updated: 2021/01/03 14:33:11 by gshona           ###   ########.fr       */
+/*   Updated: 2021/01/03 14:46:37 by jsandsla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ int		main(int ac, char **av, char **env)
 	env_t = init_env();
 	merge_env_native(env_t, env);
 	super->tkz->env_get = find_env_variable_cb_static;
+	super->tkz->last_exit_code = get_last_code;
 	super->tkz->data = env_t;
 
 	while (1)
