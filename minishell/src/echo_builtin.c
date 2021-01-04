f2cfc6db48c76a2f81819eddb0d3b56d2af784cb
Tenagrim <tenagrim@gmail.com>
2021-01-04 13:52:25 +0500

    echo builtin parse flag fix

--- minishell/src/echo_builtin.c
+++ minishell/src/echo_builtin.c
@@ -6,33 +6,63 @@
 /*   By: gshona <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 13:36:10 by gshona            #+#    #+#             */
-/*   Updated: 2021/01/03 15:01:25 by gshona           ###   ########.fr       */
+/*   Updated: 2021/01/04 13:47:51 by tenagrim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+int	skip_flags(char *const argv[], char ***first_arg)
+{
+	int	res;
+	int	i;
+	int	j;
+
+	res = 0;
+	i = 1;
+	while (argv[i])
+	{
+		if (argv[i][0] == '-' && argv[i][1])
+		{
+			j = 0;
+			while (argv[i][++j])
+				if (argv[i][j] == 'n')
+					res = 1;
+		}
+		else
+			break;
+		i++;
+	}
+	*first_arg = argv + i;
+	return (res);
+}
+
 int			echo_biultin(char *const argv[], t_env *env)
 {
 	char	min_n;
 	int		i;
+	char		**first_arg;
+	char		f;
 	(void)env;
 
-	i = 1;
-	min_n = 0;
-	if (argv[1] && !ft_strcmp("-n", argv[1]))
+
+	min_n = skip_flags(argv, &first_arg);
+	//if (argv[1] && !ft_strcmp("-n", argv[1]))
+	//{
+	//	i++;
+	//	min_n = 1;
+	//}
+	i = 0;
+	f = 0;
+	while (first_arg[i])
 	{
-		i++;
-		min_n = 1;
-	}
-	while (argv[i])
-	{
-		write(1, argv[i], ft_strlen(argv[i]));
-		if (argv[i + 1])
+		f = 1;
+		write(1, first_arg[i], ft_strlen(first_arg[i]));
+		if (first_arg[i + 1])
 			write(1, " ", 1);
 		i++;
 	}
-	if (!min_n)
+	if (!min_n && f)
 		write(1, "\n", 1);
 	return (0);
 }
