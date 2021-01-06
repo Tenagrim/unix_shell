b1b4b26fa4c5b0466fb6ce3db488afcc0b837911
Viktor Bezbatchenko <tiyez.gr@gmail.com>
2021-01-06 17:28:20 +0300

    exit_code_2_when_syntax_error

--- minishell/src/semantic/error.c
+++ minishell/src/semantic/error.c
@@ -6,7 +6,7 @@
 /*   By: jsandsla <jsandsla@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 14:07:05 by jsandsla          #+#    #+#             */
-/*   Updated: 2021/01/06 14:56:31 by jsandsla         ###   ########.fr       */
+/*   Updated: 2021/01/06 17:25:16 by jsandsla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ const char	*super_error_str(int error)
 		return ("unrecognized redirection");
 	else if (error == SUP_ERROR_INVALID_EXEC_SEPARATION)
 		return ("invalid exec separation");
-	else if (error == SUP_ERROR_INVALID_PIPE_SYNTAX)
-		return ("invalid pipe syntax");
+	else if (error == SUP_ERROR_INVALID_SYNTAX)
+		return ("invalid syntax");
 	else
 		return (tkz_error_str(error));
 }
@@ -44,7 +44,7 @@ const char	*super_error_str(int error)
 int			rechange_error(t_super *sp, int remains, int error)
 {
 	if (error == SUP_ERROR_NO_ARGUMENTS && (remains || sp->count > 0))
-		error = SUP_ERROR_INVALID_PIPE_SYNTAX;
+		error = SUP_ERROR_INVALID_SYNTAX;
 	else
 		error = SUP_SUCCESS;
 	return (error);
